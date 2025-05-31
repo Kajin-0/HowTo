e06fe33c6138c2a8f6bd8ec8bc9112615d5e02ac
Kajin-0 <28634576+Kajin-0@users.noreply.github.com>
2025-05-31 12:53:26 -0400

    Add files via upload

--- js/render.js
+++ js/render.js
@@ -4,33 +4,33 @@ fetch(`data/guides/${id}.json`)
   .then(res => res.json())
   .then(data => {
     const container = document.getElementById('guide-container');
-    const toolsHTML = `<ul class="tools-list">${data.tools.map(tool => `<li>${tool}</li>`).join('')}</ul>`;
-    const stepsHTML = `<ol>${data.steps.map(step => `<li>${step}</li>`).join('')}</ol>`;
+    const toolsHTML = '<ul class="tools-list">' + data.tools.map(tool => `<li>${tool}</li>`).join('') + '</ul>';
+    const stepsHTML = '<ol>' + data.steps.map(step => `<li>${step}</li>`).join('') + '</ol>';
     const tagsHTML = data.tags.map(tag => `<span class="badge badge-gray">${tag}</span>`).join(' ');
 
-    container.innerHTML = \`
-      <h1>\${data.title}</h1>
+    container.innerHTML = `
+      <h1>${data.title}</h1>
       <div class="meta">
-        <span class="badge badge-blue">\${data.type}</span> 
-        <span class="badge badge-green">\${data.difficulty}</span> 
-        <span class="badge badge-gray">\${data.time_minutes} min</span>
+        <span class="badge badge-blue">${data.type}</span> 
+        <span class="badge badge-green">${data.difficulty}</span> 
+        <span class="badge badge-gray">${data.time_minutes} min</span>
       </div>
 
       <hr/>
 
       <h2>Tools Required</h2>
-      \${toolsHTML}
+      ${toolsHTML}
 
       <hr/>
 
       <h2>Steps</h2>
-      \${stepsHTML}
+      ${stepsHTML}
 
       <hr/>
 
       <h2>Tags</h2>
-      <div class="tags">\${tagsHTML}</div>
-    \`;
+      <div class="tags">${tagsHTML}</div>
+    `;
 
     highlightMetrics(container);
   });
